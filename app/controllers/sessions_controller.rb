82d78fa33df4f4739e4bf31c74e5fa666508049f
Michael Hartl <michael@michaelhartl.com>
2022-02-24 21:06:54 +0000

    Implement forgetting users up to two subtle bugs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       reset_session
+      remember user
       log_in user
       redirect_to user
     else
