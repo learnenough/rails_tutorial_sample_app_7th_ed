4ba087d7a216d1c7b784ffba1b405e246898bc75
Michael Hartl <michael@michaelhartl.com>
2022-02-24 00:49:51 +0000

    Refactor tests using classes and inheritance

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,6 +5,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
+end
+
+class InvalidPasswordTest < UsersLoginTest
 
   test "login with valid email/invalid password" do
     get login_path
@@ -17,21 +20,47 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+end
 
-  test "login with valid information followed by logout" do
+class ValidLogin < UsersLoginTest
+
+  def setup
+    super
     get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
+  end
+end
+
+class ValidLoginTest < ValidLogin
+
+  test "valid login" do
     assert is_logged_in?
     assert_redirected_to @user
+  end
+
+  test "redirect after login" do
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+  end
+end
+
+class LogoutTest < ValidLogin
+
+  def setup
+    super
     delete logout_path
+  end
+
+  test "successful logout" do
     assert_not is_logged_in?
     assert_redirected_to root_url
+  end
+
+  test "redirect after logout" do
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
