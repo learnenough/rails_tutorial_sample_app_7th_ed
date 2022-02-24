e4eef280dd14112d4f5a347c595368460a6baeec
Michael Hartl <michael@michaelhartl.com>
2022-02-24 00:05:46 +0000

    Add working menu-toggle JavaScript

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,8 +1,34 @@
-let menu_nav = document.querySelector('.dropdown-toggle');
-let menu     = document.querySelector('.dropdown-menu')
+// Functions for manipulating menus.
 
-menu_nav.onclick = function(event) {
+// Returns true if we should toggle the element with the given label.
+function should_toggle(event, label) {
+  return event.target.classList.contains(label);
+}
+
+// Toggle the element with the given selector.
+function toggle(event, selector, attribute) {
+  // Stop the click from going through.      
   event.preventDefault();
   event.stopPropagation();
-  menu.classList.toggle('active');
+  // Toggle the required menu attribute.
+  let menu = document.querySelector(selector);
+  menu.classList.toggle(attribute);
 }
+
+// Defines a listener to toggle menus.
+function toggle_listener(event) {
+  // Main dropdown menu
+  if (should_toggle(event, 'dropdown-toggle')) {
+    toggle(event, '.dropdown-menu', 'active');
+  }
+  
+  // Mobile menu
+  if(should_toggle(event, 'navbar-toggle') ) {
+    toggle(event, '#example-navbar-collapse', 'collapse');
+  }
+}
+
+// Add the toggle listener to listen for clicks.
+document.addEventListener('click', function (event) {
+  toggle_listener(event);
+});
