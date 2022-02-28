b9db28763da0670ca8ebb3bee1138c81f29581ee
Michael Hartl <michael@michaelhartl.com>
2022-02-28 22:02:20 +0000

    Implement successful edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
   private
 
     def user_params
