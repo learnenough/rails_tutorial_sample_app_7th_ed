6d668b09169394795464dd52ef04c27f9d950718
Michael Hartl <michael@michaelhartl.com>
2022-03-01 21:49:42 +0000

    Activate accounts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,7 +24,7 @@ module SessionsHelper
       end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
