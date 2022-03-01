d082ca31b7fcbe0abe1e5d4e1898a44a426a73a7
Michael Hartl <michael@michaelhartl.com>
2022-03-01 21:06:42 +0000

    Configure account-activation emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: "noreply@example.com"
+  layout 'mailer'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get    '/signup', to: 'users#new'
   get    '/login',  to: 'sessions#new'
   post   '/login',  to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'  
+  delete '/logout', to: 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 end
