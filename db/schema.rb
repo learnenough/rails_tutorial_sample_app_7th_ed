6019b22a1968961a8717d8b08e6b3b5003e1797c
Michael Hartl <michael@michaelhartl.com>
2022-03-03 21:00:04 +0000

    Finish the initial Micropost model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_234818) do
+ActiveRecord::Schema.define(version: 2022_03_03_204119) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -28,4 +36,5 @@ ActiveRecord::Schema.define(version: 2022_03_01_234818) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "microposts", "users"
 end
