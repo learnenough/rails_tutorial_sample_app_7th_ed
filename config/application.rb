09d36c1b008f2dbcc6bd4e71cc3aade90a07a67a
Michael Hartl <michael@michaelhartl.com>
2022-03-04 18:26:13 +0000

    Add image resizing and configure for production

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.active_storage.variant_processor = :mini_magick
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
