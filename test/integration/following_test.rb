7ff736e9022a6cbac780197495d753302a4ad2a4
Michael Hartl <michael@michaelhartl.com>
2022-03-04 23:27:21 +0000

    Add following test, including Hotwire

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -3,7 +3,8 @@ require "test_helper"
 class FollowingTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:michael)
+    @user  = users(:michael)
+    @other = users(:archer)
     log_in_as(@user)
   end
 
@@ -22,6 +23,35 @@ class FollowingTest < ActionDispatch::IntegrationTest
     assert_match @user.followers.count.to_s, response.body
     @user.followers.each do |user|
       assert_select "a[href=?]", user_path(user)
+    end
+  end
+
+  test "should follow a user the standard way" do
+    assert_difference "@user.following.count", 1 do
+      post relationships_path, params: { followed_id: @other.id }
+    end
+  end
+
+  test "should follow a user with Hotwire" do
+    assert_difference "@user.following.count", 1 do
+      post relationships_path(format: :turbo_stream),
+           params: { followed_id: @other.id }
+    end
+  end
+
+  test "should unfollow a user the standard way" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference "@user.following.count", -1 do
+      delete relationship_path(relationship)
+    end
+  end
+
+  test "should unfollow a user with Hotwire" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference "@user.following.count", -1 do
+      delete relationship_path(relationship, format: :turbo_stream)
     end
   end
 end
