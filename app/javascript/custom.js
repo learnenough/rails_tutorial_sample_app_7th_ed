3995b994f5f9703745ab59848733a9b4e548f4fc
Michael Hartl <michael@michaelhartl.com>
2022-03-04 00:05:15 +0000

    Remove unused JS file

--- app/javascript/custom.js
+++
@@ -1,8 +0,0 @@
-let menu_nav = document.querySelector('.dropdown-toggle');
-let menu     = document.querySelector('.dropdown-menu')
-
-menu_nav.onclick = function(event) {
-  event.preventDefault();
-  event.stopPropagation();
-  menu.classList.toggle('active');
-}
