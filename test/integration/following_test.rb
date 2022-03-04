8486b6c291d2fe49cf77fd3f987044a87e3cf96b
Michael Hartl <michael@michaelhartl.com>
2022-03-04 20:00:35 +0000

    Add working follow/unfollow buttons

---
+++ test/integration/following_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class FollowingTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+    log_in_as(@user)
+  end
+
+  test "following page" do
+    get following_user_path(@user)
+    assert_not @user.following.empty?
+    assert_match @user.following.count.to_s, response.body
+    @user.following.each do |user|
+      assert_select "a[href=?]", user_path(user)
+    end
+  end
+
+  test "followers page" do
+    get followers_user_path(@user)
+    assert_not @user.followers.empty?
+    assert_match @user.followers.count.to_s, response.body
+    @user.followers.each do |user|
+      assert_select "a[href=?]", user_path(user)
+    end
+  end
+end
