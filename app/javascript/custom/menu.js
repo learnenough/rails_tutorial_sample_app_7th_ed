b1acbea207ccd9ff01fd160c0c18ee8775b9e1c4
Michael Hartl <michael@michaelhartl.com>
2022-03-03 16:29:12 -0800

    Update menu js

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,34 +1,23 @@
 // Functions for manipulating menus.
 
-// Returns true if we should toggle the element with the given label.
-function should_toggle(event, label) {
-  return event.target.classList.contains(label);
-}
-
-// Toggle the element with the given selector.
-function toggle(event, selector, attribute) {
-  // Stop the click from going through.      
-  event.preventDefault();
-  event.stopPropagation();
-  // Toggle the required menu attribute.
-  let menu = document.querySelector(selector);
-  menu.classList.toggle(attribute);
-}
-
-// Defines a listener to toggle menus.
-function toggle_listener(event) {
-  // Main dropdown menu
-  if (should_toggle(event, 'dropdown-toggle')) {
-    toggle(event, '.dropdown-menu', 'active');
-  }
-  
-  // Mobile menu
-  if(should_toggle(event, 'navbar-toggle') ) {
-    toggle(event, '#example-navbar-collapse', 'collapse');
-  }
-}
-
 // Add the toggle listener to listen for clicks.
-document.addEventListener('click', function (event) {
-  toggle_listener(event);
+document.addEventListener('click', function(event) {
+  // Main dropdown menu
+  if (event.target.classList.contains('dropdown-toggle')) {
+    // Stop the click from going through.
+    event.preventDefault();
+    event.stopPropagation();
+    // Toggle the required menu attribute.
+    let menu = document.querySelector('.dropdown-menu');
+    menu.classList.toggle('active');
+  }
+  // Mobile menu
+  if (event.target.classList.contains('navbar-toggle')) {
+    // Stop the click from going through.
+    event.preventDefault();
+    event.stopPropagation();
+    // Toggle the required menu attribute.
+    let menu = document.querySelector('#example-navbar-collapse');
+    menu.classList.toggle('collapse');
+  }
 });
