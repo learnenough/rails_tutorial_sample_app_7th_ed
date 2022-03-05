a50db08809052033ab9d8013b9cf6f54df8c6df7
Michael Hartl <michael@michaelhartl.com>
2022-03-05 15:03:29 -0800

    Update menu JS

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,23 +1,20 @@
-// Functions for manipulating menus.
+// Menu manipulation
 
-// Add the toggle listener to listen for clicks.
-document.addEventListener("click", function(event) {
-  // Main dropdown menu
-  if (event.target.classList.contains("dropdown-toggle")) {
-    // Stop the click from going through.
+// Add toggle listeners to listen for clicks.
+document.addEventListener("turbo:load", function() {
+  let dropdown = document.querySelector("#dropdown-toggle");
+  dropdown.addEventListener("click", function(event) {
     event.preventDefault();
     event.stopPropagation();
-    // Toggle the required menu attribute.
-    const menu = document.querySelector(".dropdown-menu");
+    let menu = document.querySelector("#dropdown-menu")
     menu.classList.toggle("active");
-  }
-  // Mobile menu
-  if (event.target.classList.contains("navbar-toggle")) {
-    // Stop the click from going through.
+  });
+
+  let hamburger = document.querySelector("#hamburger");
+  hamburger.addEventListener("click", function(event) {
     event.preventDefault();
     event.stopPropagation();
-    // Toggle the required menu attribute.
-    const menu = document.querySelector("#example-navbar-collapse");
+    let menu = document.querySelector("#navbar-menu");
     menu.classList.toggle("collapse");
-  }
+  });
 });
