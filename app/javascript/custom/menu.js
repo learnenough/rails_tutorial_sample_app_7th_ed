5845f50bc27bbce88deb977cc33bf6ed46a903c6
Michael Hartl <michael@michaelhartl.com>
2022-03-05 16:59:43 -0800

    Add working touchstart listener

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -2,19 +2,25 @@
 
 // Add toggle listeners to listen for clicks.
 document.addEventListener("turbo:load", function() {
-  let dropdown = document.querySelector("#dropdown-toggle");
-  dropdown.addEventListener("click", function(event) {
+  let hamburger = document.querySelector("#hamburger");
+  hamburger.addEventListener("touchstart", function(event) {
     event.preventDefault();
     event.stopPropagation();
-    let menu = document.querySelector("#dropdown-menu")
-    menu.classList.toggle("active");
+    let menu = document.querySelector("#navbar-menu");
+    menu.classList.toggle("collapse");
   });
-
-  let hamburger = document.querySelector("#hamburger");
   hamburger.addEventListener("click", function(event) {
     event.preventDefault();
     event.stopPropagation();
     let menu = document.querySelector("#navbar-menu");
     menu.classList.toggle("collapse");
+  });
+
+  let account = document.querySelector("#account");
+  account.addEventListener("click", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    let menu = document.querySelector("#dropdown-menu")
+    menu.classList.toggle("active");
   });
 });
