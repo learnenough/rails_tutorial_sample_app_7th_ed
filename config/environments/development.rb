aa5907d7d9b3a8dc1d7b88195d4a421fff0d6d3e
Michael Hartl <michael@michaelhartl.com>
2022-04-08 12:32:13 -0700

    Add hello

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Allow connections to local server on cloud IDE.
+  config.hosts.clear
 end
