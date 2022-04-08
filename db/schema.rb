d291f5ee992f9f4479254e9f1b741686556776f2
Michael Hartl <michael@michaelhartl.com>
2022-04-08 14:07:44 -0700

    Add password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_08_210133) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_08_210711) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_08_210133) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,15 +1,13 @@
 require "test_helper"
 
-class UsersSignup < ActionDispatch::IntegrationTest
+class UsersSignupTest < ActionDispatch::IntegrationTest
 
   def setup
     ActionMailer::Base.deliveries.clear
   end
-end
-
-class UsersSignupTest < UsersSignup
 
   test "invalid signup information" do
+    get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name:  "",
                                          email: "user@invalid",
@@ -22,6 +20,7 @@ class UsersSignupTest < UsersSignup
   end
 
   test "valid signup information with account activation" do
+    get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
                                          email: "user@example.com",
@@ -29,42 +28,20 @@ class UsersSignupTest < UsersSignup
                                          password_confirmation: "password" } }
     end
     assert_equal 1, ActionMailer::Base.deliveries.size
-  end
-end
-
-class AccountActivationTest < UsersSignup
-
-  def setup
-    super
-    post users_path, params: { user: { name:  "Example User",
-                                       email: "user@example.com",
-                                       password:              "password",
-                                       password_confirmation: "password" } }
-    @user = assigns(:user)
-  end
-
-  test "should not be activated" do
-    assert_not @user.activated?
-  end
-
-  test "should not be able to log in before account activation" do
-    log_in_as(@user)
+    user = assigns(:user)
+    assert_not user.activated?
+    # Try to log in before activation.
+    log_in_as(user)
     assert_not is_logged_in?
-  end
-
-  test "should not be able to log in with invalid activation token" do
-    get edit_account_activation_path("invalid token", email: @user.email)
+    # Invalid activation token
+    get edit_account_activation_path("invalid token", email: user.email)
     assert_not is_logged_in?
-  end
-
-  test "should not be able to log in with invalid email" do
-    get edit_account_activation_path(@user.activation_token, email: 'wrong')
+    # Valid token, wrong email
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
     assert_not is_logged_in?
-  end
-
-  test "should log in successfully with valid activation token and email" do
-    get edit_account_activation_path(@user.activation_token, email: @user.email)
-    assert @user.reload.activated?
+    # Valid activation token
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
