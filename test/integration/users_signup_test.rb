c38f93687f6e5a4e0b78816df8836f0536ba289a
Michael Hartl <michael@michaelhartl.com>
2022-04-09 11:22:24 -0700

    Add test for unprocessable entity

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,11 +10,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
+    assert_response :unprocessable_entity
     assert_template 'users/new'
   end
 
   test "valid signup information" do
-    get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
                                          email: "user@example.com",
