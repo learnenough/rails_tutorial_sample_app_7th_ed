26fc082146665bafb5723c3555de2bdf14d88fe8
Michael Hartl <michael@michaelhartl.com>
2022-04-08 18:42:54 -0700

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,10 @@
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 # Generate a bunch of additional users.
 99.times do |n|
@@ -12,5 +15,7 @@ User.create!(name:  "Example User",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
