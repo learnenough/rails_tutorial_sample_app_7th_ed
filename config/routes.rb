3564b6c0502b6a6156342696f27ab3764508f297
Michael Hartl <michael@michaelhartl.com>
2022-05-30 20:37:52 -0700

    Fix refresh error

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,5 @@ Rails.application.routes.draw do
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
   resources :relationships,       only: [:create, :destroy]
+  get '/microposts', to: 'static_pages#home'
 end
