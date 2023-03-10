32bc15769c13c5c7e2bebffadd911aa1859a803a
Pete Cheslock <petecheslock@gmail.com>
2023-03-10 12:27:19 -0500

    Add Devcontainer

---
+++ .devcontainer/postcreate.sh
@@ -0,0 +1,8 @@
+#bundle install
+#curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+#echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+bundle install --without production
+#yarn install --check-files
+bundle exec rails db:drop
+bundle exec rails db:migrate
+bundle exec rails db:seed
