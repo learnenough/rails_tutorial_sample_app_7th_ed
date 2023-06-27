7979f3e051b62d596c11d8926f974a3e64f44f85
Pete Cheslock <petecheslock@gmail.com>
2023-06-27 10:13:02 -0700

    Commit breaking change

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,6 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
-  resources :relationships,       only: [:create, :destroy]
+  resources :relationsips,       only: [:create, :destroy]
   get '/microposts', to: 'static_pages#home'
 end
