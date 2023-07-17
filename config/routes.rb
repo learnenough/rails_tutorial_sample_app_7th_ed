6cc6c219d0c46f3f7dc8540e2c3bda8f6e3ac8c2
Adam Trotta <ahtrotta@gmail.com>
2023-07-17 15:49:55 -0400

    test

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts,          only: [:create, :destroy]
   resources :relationships,       only: [:create, :destroy]
   get '/microposts', to: 'static_pages#home'
 end
